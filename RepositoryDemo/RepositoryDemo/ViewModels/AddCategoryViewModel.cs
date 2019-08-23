fa511ccb76c0b6a1029f86e967c4d69e6d6bb987
jese.alvarado <jese.alvarado@CDS3UNIVO8>
2019-08-23 15:58:46 -0600

    AddCategory view and ViewModel added

---
+++ RepositoryDemo/RepositoryDemo/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using RepositoryDemo.Models;
+using RepositoryDemo.Services;
+using Xamarin.Forms;
+
+namespace RepositoryDemo.ViewModels
+{
+  public class AddCategoryViewModel
+    {
+        ICategoryRepository Repository;
+        public AddCategoryViewModel(ICategoryRepository repository)
+        {
+            Repository = repository;
+        }
+
+        #region Properties
+        public ObservableCollection<Product> Products { get; set; }
+        public string CategoryName { get; set; }
+        public string TextProductName { get; set; }
+
+        public string Description { get; set; }
+        #endregion
+
+        #region Commands
+        public Command AddProductCommand { get; set; }
+        public Command SaveCategoryCommand { get; set; }
+        #endregion
+    }
+}
