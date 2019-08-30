83ebd1a879648c90dc0323e81750cbf544c6f535
jese.alvarado <jese.alvarado@CDS3UNIVO14>
2019-08-30 16:00:05 -0600

    View ListCategories added

---
+++ RepositoryDemo/RepositoryDemo/ViewModels/ListCategoriesViewModel.cs
@@ -0,0 +1,34 @@
+﻿using RepositoryDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using RepositoryDemo.Services;
+
+namespace RepositoryDemo.ViewModels
+{
+ public  class ListCategoriesViewModel
+    {
+        private readonly ICategoryRepository repository;
+
+      
+        public ListCategoriesViewModel(ICategoryRepository repository )
+        {
+            this.repository = repository;
+
+        }
+        #region Properties
+        public List<Category> Categories { get; set; }
+        public Category SelectedCategory { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
---
+++ RepositoryDemo/RepositoryDemo/Views/ListCategories.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RepositoryDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListCategories : ContentPage
+    {
+        public ListCategories()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
